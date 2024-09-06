0bea6e144b78e8e4e3d3b6c945297538e696d348
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2024-09-06 23:44:04 +0545

    Learned to create varaiables

---
+++ 1.Home.py
@@ -0,0 +1,9 @@
+print("Hello")
+if 5 < 2:
+ print("Hello I am here")
+else:
+  print("I am not here")
+a = 5
+b = "hello"
+print(a)
+print(b)
---
+++ 2.Variables.py
@@ -0,0 +1,26 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+a = str(3)
+b = str(3)  #Casting
+c = int(2)
+print(a)
+print(b)
+print(c)
+
+if a > b:
+    print("A is greater than B")
+else:
+    print("B is Greater than a")
+
+k,l,m = "Uday", "Saugat", "Prasanna"
+
+print(k,l,m)
+print(k)
+print(l)
+print(m)
+
+o=p=q = "Ramesh"
+print(o,p,q)
+
