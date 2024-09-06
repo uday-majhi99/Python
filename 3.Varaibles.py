3a59c066898baaa1b5d427a0d46acc9ca5c0238c
Uday Kumar Majhi <u.k.majhi1@gmail.com>
2024-09-07 00:02:56 +0545

    Added new file

--- 3.Varaibles.py
+++ 3.Varaibles.py
@@ -1,2 +1,18 @@
 x = "Python is awesome"
+y = "Python is awesome"
+z = "Python is awesome"
 print(x)
+print(x,y,z)
+print(x+y+z)
+
+def myfunc():
+    x ="fantastic"
+    global y
+    y = "fantastic"
+    print("Yes,"+x)
+    print("Yes,"+y)
+
+myfunc()
+
+print("Python ishdbfkhss " +x)
+print("Python iskjhdgfiuygss " +y)
